e7d35b0b43f3b26f2f4d042cc2d9cae3464db5f6
anmolahlawat <anmolahlawat007@gmail.com>
2026-03-11 21:24:40 +0530

    Day 1 Python basics practice

---
+++ python-basics/day1.py
@@ -0,0 +1,74 @@
+print("hello world")
+
+# variables 
+name = "anmol"
+age = 19
+
+# printing variables 
+
+
+print(name)
+print(age)
+
+
+
+
+# basic mathematical operations 
+
+
+a = 15
+b = 20
+c = 2 
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**c)
+
+# if/else satatements
+
+
+num = 12
+
+if num > 5:
+    print("number greater 5")
+else:
+    print("number smaller than 12")
+
+
+
+# for loop
+
+for i in range(5):
+    print(i)
+
+print()
+print()
+print()
+
+
+
+for j in range(1,6):
+    print(j)
+
+
+
+
+# lists in python
+
+
+
+lst = [1,2,3,4,5,56,70]
+lst2 = ["anmol",19,"dsa"]
+
+print(lst)
+print(lst2)
+
+
+for i in lst2:
+    print("learning : " + lst2[2])
+
+print("name : " + lst2[0])
+
